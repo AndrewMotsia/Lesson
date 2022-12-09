a8ffcb68a0266bff3ca44c471764d689235d3334
MAR <120112462+AndrewMotsia@users.noreply.github.com>
2022-12-09 12:21:05 +0200

    Add files via upload

---
+++ Text1/Text1/Text1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	int age;
+	double weight;
+	std::cout << "input age: ";
+	std::cin >> age;
+	std::cout << "input weigth: ";
+	std::cin >> weight;
+	std::cout << "Your age: " << age << "\t your weight: " << weight << std::endl;
+	return 0;
+
+}
